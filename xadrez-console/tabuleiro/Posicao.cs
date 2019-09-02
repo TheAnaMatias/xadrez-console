b3762da7cb2e23fdc4525c6a08ee0c948ee2a184
Ana Matias <anamatias@ipvc.pt>
2019-09-02 01:37:49 +0100

    Testes inciais com movimentos possíveis das peças

--- xadrez-console/tabuleiro/Posicao.cs
+++ xadrez-console/tabuleiro/Posicao.cs
@@ -20,6 +20,12 @@ namespace tabuleiro
             this.coluna = coluna;
         }
 
+        public void definirValores(int linha, int coluna)
+        {
+            this.linha = linha;
+            this.coluna = coluna;
+        }
+
         public override string ToString()
         {
             return "Posição: " + linha + ", " + coluna;
